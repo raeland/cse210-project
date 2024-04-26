8791ed698512ba5f7da657426e26addc9206885f
raeland <rsvarady@gmail.com>
2024-04-25 21:35:32 -0400

    W01 prep assignments

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,7 +3,13 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
+    {   
+        Console.Write("What's your first name?");
+        string first = Console.ReadLine();
+
+        Console.Write("What's your last name?");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }

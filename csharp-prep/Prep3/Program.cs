351341873eb7d2f4c6b17273b12ed2495fdd1c89
raeland <rsvarady@gmail.com>
2024-05-01 21:40:02 -0400

    loops with one stretch :)

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+        // wrong place - moved to while loop - Console.WriteLine("What is your guess? ");
+
+        //#3 the random number part
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        //int guess = 3; //used for core assignments
+        int guessCount = 0; //for guess counter in stretch assignment
+
+        while (true)//(guess != magicNumber) //used for core assignments
+        {
+            Console.Write("What is your guess? ");
+            //guess = int.Parse(Console.ReadLine()); //used for core assignments
+            int guess = int.Parse(Console.ReadLine()); //for stretch assignment
+            guessCount++; //for stretch assignment
+
+            if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower ");
+            }
+                else if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher ");
+                }
+                    else
+                    {
+                        Console.WriteLine("You Guessed Correctly! ");
+                        break;
+                    }
+        }        
+        Console.WriteLine($"And you did it in {guessCount} tries!!! ");//for stretch assignment
     }
 }

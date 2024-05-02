c858c07f8a04371a0325f381789fb48736760580
raeland <rsvarady@gmail.com>
2024-05-01 22:09:16 -0400

    started but too tired to think anymore...

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Please type a series of numbers. Type 0 when finished. ");
+        Console.WriteLine("Enter number: ");
+        Console.Read();
     }
+    //1. compute the sum, or total, of the numbers in the list
+    //2. compute the average of the numbers in the list
+    //3. find the maximum, or largest, number in the list
 }

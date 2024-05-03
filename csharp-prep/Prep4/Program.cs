8b919ea2a6f81b96434f89133b9245b88cf13b0c
raeland <rsvarady@gmail.com>
2024-05-02 20:55:21 -0400

    w02 steps 1-3 completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,14 +1,49 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please type a series of numbers. Type 0 when finished. ");
-        Console.WriteLine("Enter number: ");
-        Console.Read();
-    }
+        List<int> numbers = new List<int>();
+
+        Console.Write("Please type a series of numbers. Type 0 when finished. ");
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            
+            string userEntry = Console.ReadLine();
+            userNumber = int.Parse(userEntry);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
     //1. compute the sum, or total, of the numbers in the list
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number; 
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
     //2. compute the average of the numbers in the list
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
     //3. find the maximum, or largest, number in the list
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
+    }
 }

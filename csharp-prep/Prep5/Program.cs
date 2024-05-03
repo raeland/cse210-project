7e97c74f6888ba6882b1326fe214273ef8dd10a4
raeland <rsvarady@gmail.com>
2024-05-02 22:42:14 -0400

    first draft of core assignment requirements

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Welcome to the Program!");
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number: ");
+        string favoriteNumber = Console.ReadLine();
+        
+        favoriteNumber = int.Parse(favoriteNumber);
+        
+        int sq = 1;
+
+        while (sq < favoriteNumber / sq)
+        {
+            sq++;
+        }
+        if (sq > favoriteNumber / sq)
+            return sq - 1;
+            return sq;
     }
-}
+        Console.WriteLine($"Your name is {name}, the square of your favorite number {favoriteNumber} is {square}.");
+    }

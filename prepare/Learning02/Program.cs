7ec2c0e313461b49de638776baa92f23467b6df9
raeland <rsvarady@gmail.com>
2024-05-09 21:28:44 -0400

    updated resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,19 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._jobTitle = "Carb Extrodinaire";
+        job1._company = "Hamburger Haven";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
 
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._jobTitle = "Snuggle Pup";
+        job2._company = "Sanctuary Taco";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Athena Burrito";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);

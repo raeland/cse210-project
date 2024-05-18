12a92a9fb3cae73fb60743c2318895e2d9459bbf
raeland <rsvarady@gmail.com>
2024-05-18 11:51:44 -0400

    moved MENU to journal Switch Case

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -4,10 +4,44 @@ namespace Journal
 {
     public class Journal
 {
-    public string _menu;
-    public string _entry;
-    public string _prompt;
-    public int _date;
+    public static void Menu()
+    {
+        {
+            Console.WriteLine("What would you like to do? ");
+            Console.WriteLine("1. Entry ");
+            Console.WriteLine("2. Display ");
+            Console.WriteLine("3. Load ");
+            Console.WriteLine("4. Save ");
+            Console.WriteLine("5. Quit ");
+            Console.WriteLine("Type the number corresponding to your choice: ");
+        }
 
+        int input = 1;
+        input = Console.Read();
+        switch(input)
+        {
+            case 1:
+                Console.   
+                break;
+            case 2:
+                Console.   
+                break;
+            case 3:
+                Console.   
+                break;
+            case 4:
+                Console.   
+                break;
+            case 5:
+                Console.   
+                break;
+            default 5:
+                Console.WriteLine("Quit");
+                return;
+                //break is a requirement in C#
+                //or return; -breaks from the main method. 
+                //the return statement always goes outside whatever method you are in. 
+        }
+        //input = Console.Read();
 }
 }
--- prove/Develop02/Prompt.cs
+++ prove/Develop02/Prompt.cs
@@ -2,14 +2,21 @@ using System;
 
 namespace Journal
 {
-    public class Prompt
+    public class PromptGenerator
 {
 
-/*Console.WriteLine("What is one thing you would re-do from today (good or bad), if you could?");
-Console.WriteLine("Describe the most diTicult part of your day?");
-Console.WriteLine("Did you make anyone smile today? Describe the exchange");
-Console.WriteLine("Where did you see the hand of the Lord in your life today?");
-Console.WriteLine("Did you learn something important today?");
-*/
+private List<string> prompt;
+
+public PromptGenerator()
+{
+    prompt = new List<string>
+    {
+        "What is one thing you would re-do from today (good or bad), if you could?",
+        "Describe the most diTicult part of your day?",
+        "Did you make anyone smile today? Describe the exchange",
+        "Where did you see the hand of the Lord in your life today?",
+        "Did you learn something important today?"
+    }
+}
 }
 }

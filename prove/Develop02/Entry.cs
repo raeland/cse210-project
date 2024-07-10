aec43c1ebb59d2297150a5845ef4bb1353c9c421
raeland <rsvarady@gmail.com>
2024-07-09 21:23:11 -0400

    entry again giv'n me problems

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -8,16 +8,24 @@ namespace Journal
     public string _response { get; set; }
     public DateTime _date { get; set; }
 
-    public Entry(string prompt, string response)
-    {
-      _entry = prompt;
-      _response = response;
-      _date = DateTime.Now;
+    // public Entry(string prompt, string response)
+    // {
+    //   _entry = prompt;
+    //   _response = response;
+    //   _date = DateTime.Now;
+    public static Entry Prompt() 
+    { 
+      Prompt prompt = new Prompt(); 
+      string entry = prompt.PromptGenerator(); 
+      Console.Write($"{entry} "); 
+      string response = Console.ReadLine(); 
+      return new Entry(entry, response); 
     }
 
-    public override string ToString()
-    {
-      return $"Date: {_date}\nPrompt: {_entry}\nResponse: {_response}\n";
-    }
+    // public override string ToString()
+    // {
+    //   return $"Date: {_date}\nPrompt: {_entry}\nResponse: {_response}\n";
+    // }
   }
 }
+
